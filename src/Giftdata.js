0a76e1d524a6b0458b71c3936cb3d097d6fd3b9f
Isabbelle <isabelle.muresan@gmail.com>
2021-05-25 20:37:02 +0200

    Added components

---
+++ src/Giftdata.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+export const GiftData = [
+    {
+        isLego: true,
+        isStarWars: false,
+        hasKid: true,
+        link:
+            'https://www.amazon.com/LEGO-BrickHeadz-Potter-Building-Multicolor/dp/B07BGMJ38R/ref=sr_1_19?dchild=1&keywords=harry+potter+lego+children&qid=1620759295&sr=8-19',
+    },
+    {
+        isLego: true,
+        isStarWars: false,
+        hasKid: false,
+        link:
+            'https://www.amazon.com/LEGO-Childrens-Collectible-Role-Playing-Dollhouse/dp/B0858VNX4N/ref=sr_1_1?dchild=1&keywords=harry+potter+lego+children&qid=1620759295&sr=8-1',
+    },
+    {
+        isLego: true,
+        isStarWars: true,
+        hasKid: true,
+        link:
+            'https://www.amazon.com/LEGO-BrickHeadz-Mandalorian-Featuring-Buildable/dp/B0849S7C5W/ref=sr_1_2?dchild=1&keywords=star+wars+lego+children&qid=1620759276&sr=8-2',
+    },
+    {
+        isLego: true,
+        isStarWars: true,
+        hasKid: false,
+        link:
+            'https://www.amazon.com/LEGO-Helmet-Building-Collectible-Character/dp/B083JX5M79/ref=pd_sbs_7?pd_rd_w=qKUHk&pf_rd_p=651d64d1-3c73-45b6-ae09-e545600e3a22&pf_rd_r=CDTABZFKD5AH7782ZDNM&pd_rd_r=758e39c5-08aa-4d20-b3e8-d8c8377a7e1d&pd_rd_wg=fnUdC&pd_rd_i=B083JX5M79&psc=1',
+    },
+    {
+        isLego: false,
+        isStarWars: false,
+        hasKid: true,
+        link:
+            'https://www.amazon.com/Spoontiques-Harry-Potter-Ceramic-Camper/dp/B08DL7N67T/ref=sr_1_10?dchild=1&keywords=harry+potter+mug&qid=1620758993&sr=8-10',
+    },
+    {
+        isLego: false,
+        isStarWars: false,
+        hasKid: false,
+        link:
+            'https://www.amazon.com/Buffalo-HP113932Z-Hogwarts-Splatter-Multicolor/dp/B06XJ4B2PR/ref=sr_1_5?dchild=1&keywords=harry+potter+mug&qid=1620758993&sr=8-5',
+    },
+    {
+        isLego: false,
+        isStarWars: true,
+        hasKid: true,
+        link:
+            'https://www.amazon.com/Zak-Reusable-Leak-Proof-Designs-Chewbacca/dp/B07V4WLHCY/ref=sr_1_22?dchild=1&keywords=star+wars+mug+kids&qid=1620759096&sr=8-22',
+    },
+    {
+        isLego: false,
+        isStarWars: true,
+        hasKid: false,
+        link:
+            'https://www.amazon.com/Spoontiques-Harry-Potter-Ceramic-Camper/dp/B08DL7N67T/ref=sr_1_10?dchild=1&keywords=harry+potter+mug&qid=1620758993&sr=8-10',
+    },
+]
