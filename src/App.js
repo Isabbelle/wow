ad3b7a9b36231eca3605cfd5792ed976e85f3d97
Isabbelle <isabelle.muresan@gmail.com>
2021-05-25 20:37:26 +0200

    added components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+
+
+
+
 function App() {
+
+
+  
+  const [isLego, setIsLego] = useState(true);
+  const [isStarWars, setIsStarWars] = useState(false)
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1 className="question">Is your colleague a Lego fan?</h1>
+      
     </div>
   );
 }
